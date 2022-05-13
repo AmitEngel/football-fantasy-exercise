e256a3a34306d58f0735432d78ddbf4ff60db36a
AmitEngel <amiten9@gmail.com>
2022-05-13 13:05:42 +0300

    latest after class

--- controllers/user.ctrl.js
+++ controllers/user.ctrl.js
@@ -1,28 +1,4 @@
 const express = require('express');
-<<<<<<< HEAD
-const dal = require('./../DAL');
-const uuidv4 = require('uuid').v4;
-const router = express.Router();
-
-router.get('/', function (req, res) {
-    const users = dal.read('user');
-    res.send(users);
-});
-
-router.post('/', function (req, res) {
-    console.log(req.body)
-    const user = {
-        id: uuidv4(),
-        first: req.body['first'],
-        last: req.body['last'],
-        email: req.body['email'],
-        password: req.body['password'],
-        playerIds: []
-    }
-    dal.add('user', user);
-    res.send(user);
-})
-=======
 const dal = require('../DAL');
 
 const router = express.Router();
@@ -32,6 +8,5 @@ router.get('/:id', function (req, res) {
     delete user['password'];
     res.send(user);
 });
->>>>>>> afbb2508e5e10b249f1e9c5add21ecca392bdf69
 
 module.exports = router;
--- index.js
+++ index.js
@@ -1,44 +1,3 @@
-<<<<<<< HEAD
-const express = require('express');
-const app = express();
-const path = require('path');
-const bp = require("body-parser");
-const uuid = require('uuid').v4;
-const typedefs = require('./typedefs');
-app.use(bp.urlencoded({ extended: true }));
-
-const PORT = 3456;
-
-app.get('/api/player', function (req, res) {
-    res.sendFile(path.join(__dirname , './data/players.json'));
-});
-app.get('/api/team', function (req, res) {
-    res.sendFile(path.join(__dirname , './data/teams.json'));
-})
-app.get('/api/player/position', function (req, res) {
-    res.send(["Forward",
-        "Midfielder",
-        "Defender",
-        "Goalkeeper"]);
-});
-
-app.post('/api/player', function (req, res) {
-/**@type {typedefs.FootballPlayer} */
-    const player = {
-        id: uuid(),
-        name: req.body['player-name'],
-        team: req.body['player-team'],
-        position: req.body['player-position']
-    }
-    res.send(player);
-    console.log(req.body);
-})
-
-
-app.use('/', express.static('public'));
-
-app.listen(PORT, () => console.log(`server started at port ${PORT}`));
-=======
 const express = require('express');
 const app = express();
 const PORT = 8989;
@@ -63,4 +22,3 @@ app.use('/api/user', userCtrl);
 app.use('/api/auth', authCtrl);
 
 app.listen(PORT, () => console.log(`server started at port ${PORT}`))
->>>>>>> afbb2508e5e10b249f1e9c5add21ecca392bdf69
